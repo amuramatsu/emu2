5c5e801511b06d3448da72768c0a514ea9023187
MURAMATSU Atsushi <amura@tomato.sakura.ne.jp>
2022-04-05 16:47:55 +0900

    add helf for LIM-EMS

--- src/dbg.c
+++ src/dbg.c
@@ -32,9 +32,10 @@ void print_usage(void)
            "\t\t      point to the unix working directory.\n"
            "  %-18s  Set DOS code-page. Set to '?' to show list of code-pages.\n"
            "  %-18s  Limit DOS memory to 512KB, fixes some old buggy programs.\n"
-           "  %-18s  Specifies a DOS append paths, separated by ';'.\n",
+           "  %-18s  Specifies a DOS append paths, separated by ';'.\n"
+           "  %-18s  Use LIM-EMS 4.0. Set this variable as available pages.\n",
            prog_name, ENV_DBG_NAME, ENV_DBG_OPT, ENV_PROGNAME, ENV_DEF_DRIVE, ENV_CWD,
-           ENV_DRIVE "n", ENV_CODEPAGE, ENV_LOWMEM, ENV_APPEND);
+           ENV_DRIVE "n", ENV_CODEPAGE, ENV_LOWMEM, ENV_APPEND, ENV_EMSMEM);
     exit(EXIT_SUCCESS);
 }
 
